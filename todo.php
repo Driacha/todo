21c5870337063420e9023598753bac41aaab8f53
Julia Fleischer <fleischer.julia@web.de>
2016-10-19 08:55:14 +0200

    Change id and fix spelling

--- todo.php
+++ todo.php
@@ -10,22 +10,22 @@ href="todo.css">
 
 </head>
 <body>
-<div class="task-list" id="ausstehend">
+<div class="task-list" id="pending">
  <h3>Ausstehend</h3>
 </div>
 
-<div class="task-list" id="inbearbeitung">
+<div class="task-list" id="inProgress">
  <h3>In Bearbeitung</h3>
 </div>
 
-<div class="task-list" id="beendet">
+<div class="task-list" id="completed">
     <h3>Beendet</h3>
 </div>
 
 <div class="task-list">
  <h3>Füge eine Aufgabe hinzu</h3>
     <form id="todo-form">
-        <input type="text" placeholder="bezeichnung"/>
+        <input type="text" placeholder="Bezeichnung"/>
         <textarea type="text" placeholder="Beschreibung"></textarea>
         <input type="text" placeholder="Frist (tt/mm/jjjj)"/>
         <input type="button" class="btn btn-primary" value="Hinzufügen"/>
